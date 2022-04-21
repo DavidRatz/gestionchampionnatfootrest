c914a6cb682086adc9cc6bf8e909af9b48464b3a
David Ratz <david.ratz@hotmail.com>
2022-04-21 16:57:33 +0200

    Journee update

--- src/main/java/be/technifutur/gestionchampionnatfootrest/data/repo/JourneeRepository.java
+++ src/main/java/be/technifutur/gestionchampionnatfootrest/data/repo/JourneeRepository.java
@@ -9,6 +9,6 @@ import be.technifutur.gestionchampionnatfootrest.models.entities.Journee;
 
 public interface JourneeRepository extends JpaRepository<Journee,Long> {
     Optional<Journee> findByNumero(int numero);
-    @Query(value = "select * from journee j join rencontre r on r.journee_id = j.id join club c on c.id = r.club_domicile_id join championnat ch on ch.id = j.championnat_id where j.numero = ?1 and j.championnat_id = ?2 order by r.date", nativeQuery = true)
+    @Query(value = "select * from journee j join rencontre r on r.journee_id = j.id join club c on c.id = r.club_domicile_id join championnat ch on ch.id = j.championnat_id where j.numero = ?1 and j.championnat_id = ?2 order by r.date asc", nativeQuery = true)
     Optional<Journee> findByNumeroAndChampionnat(int numero, Long championnat);
 }
